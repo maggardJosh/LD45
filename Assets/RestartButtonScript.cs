a8b81eca04eb8074a91337bfe5a2b9a942b28388
Josh Maggard <jmaggard62@gmail.com>
2019-10-06 12:46:43 -0500

    Add Reset Button. Closes #19

---
+++ Assets/RestartButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButtonScript : MonoBehaviour
+{
+    public void Restart()
+    {
+        AudioManager.PlayOneShot(GameSettings.ResetSFX);
+        LoadingScreen.Show(() => SceneManager.LoadScene(SceneManager.GetActiveScene().name));
+    }
+}

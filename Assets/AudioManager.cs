753a0f2a1349ce236f870c71b8bae4851082480c
Josh Maggard <jmaggard62@gmail.com>
2019-10-06 02:46:08 -0500

    Add Music and audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager _instance;
+    private static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<AudioManager>();
+            return _instance;
+        }
+    }
+
+    public AudioSource sfxAudioSource;
+    public AudioSource musicAudioSource;
+
+    private bool MuteMusic
+    {
+        get
+        {
+            return PlayerPrefs.GetInt("MuteMusic", 0) == 1;
+        }
+        set
+        {
+            PlayerPrefs.SetInt("MuteMusic", value ? 1 : 0);
+        }
+    }
+
+    void Awake()
+    {
+        _instance = this;
+        musicAudioSource.volume = MuteMusic ? 0 : 1;
+    }
+
+    public static void PlayOneShot(AudioClip ac)
+    {
+        Instance.sfxAudioSource.PlayOneShot(ac);
+    }
+
+    public void ToggleMusicSound()
+    {
+        MuteMusic = !MuteMusic;
+        Instance.musicAudioSource.volume = MuteMusic ? 0 : 1;
+    }
+    
+}
---
+++ Assets/EssentialsLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EssentialsLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (LoadingScreen.Instance == null)
+            Instantiate(GameSettings.EssentialsPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

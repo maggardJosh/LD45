ddd5df3d520b4ae47985e681770813a273c0b8b6
Josh Maggard <jmaggard62@gmail.com>
2019-10-06 03:20:54 -0500

    Very basic level select. Closes #3

---
+++ Assets/LevelSelectButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelectButton : MonoBehaviour
+{
+    public string sceneName = "";
+    public LevelSelectScreen screen;
+
+    public void LoadLevel()
+    {
+        screen.gameObject.SetActive(false);
+        LoadingScreen.Show(() => SceneManager.LoadScene(sceneName));
+    }
+
+    public void InitButton(string sceneName, LevelSelectScreen screen)
+    {
+        this.screen = screen;
+        GetComponentInChildren<Text>().text = sceneName;
+        this.sceneName = sceneName;
+    }
+}
---
+++ Assets/LevelSelectScreen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectScreen : MonoBehaviour
+{
+    public Transform LevelSelectContainer;
+    public GameObject LevelSelectButtonPrefab;
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+        UpdateLevels();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateLevels()
+    {
+        foreach(var scene in GetScenes())
+        {
+            var go = Instantiate(LevelSelectButtonPrefab, LevelSelectContainer);
+            var lsb = go.GetComponent<LevelSelectButton>();
+            lsb.InitButton(scene, this);
+        }
+    }
+
+    private static string[] GetScenes()
+    {
+        List<string> result = new List<string>();
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            string sceneName = System.IO.Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+            result.Add(sceneName);
+        }
+        return result.ToArray();
+    }
+
+}
